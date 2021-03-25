ee1d9a25d5c0b9c1caf4513777e0d52bd052c053
hoangvutruong513 <hoangvu.truong@rovisys.com>
2021-03-25 13:37:07 +0800

    Fundamentals Unit Testing

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        [Ignore("Because I wanted to!")]
+        public void Add_WhenCalled_ReturnsSumofArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(2, 2, 2)]
+        public void Max_WhenCalled_ReturnsTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
