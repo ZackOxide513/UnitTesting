dd6e79626e7bf372a39fefaad639efe9c192c51c
hoangvutruong513 <hoangvu.truong@rovisys.com>
2021-03-25 17:27:47 +0800

    void, Exception, Event Testing

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger _errorLogger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _errorLogger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty()
+        {
+            _errorLogger.Log("a");
+
+            Assert.That(_errorLogger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgNullException(string error)
+        {
+            Assert.That(() => _errorLogger.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var id = Guid.Empty;
+            _errorLogger.ErrorLogged += (sender, args) =>
+            {
+                id = args;
+            };
+            _errorLogger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+    }
+}
